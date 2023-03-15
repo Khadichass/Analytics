3ad6e6b718c327a254ae4c157aa9380aaa106341
Mahliyo <budur1202@gmail.com>
2023-03-15 17:26:03 +0300

    calendar1

--- script.js
+++ script.js
@@ -1,19 +1,16 @@
 // const currentDate = document.getElementsByTagName("li");
+"use script"
 
-const currentDate = document.getElementsByTagName("li");
+const currentDate = document.querySelector(".click__li");
 
 let hiii = document.getElementById("hiii");
 
-const calendarLi = document.getElementsByClassName("click__li");
-
-currentDate.addEventListener("click", function () {
-  calendarLi.style.backgroundColor = "#0088F0";
+currentDate.addEventListener("click", () => {
+  currentDate.style.backgroundColor = "#0088F0";
   calendarLi.style.color = "#fff";
   calendarLi.style.borderRadius = "50%";
   calendarLi.style.width = "20px";
   calendarLi.style.height = "40px";
-
-  console.log("hello");
 });
 
 hiii.addEventListener("click", function () {
