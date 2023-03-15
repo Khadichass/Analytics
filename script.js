9e2f99a3db33682a0a86dd4e697f12cbd64fb21e
Khadicha <246abdurashidova123@gmail.com>
2023-03-15 18:19:14 +0500

    changes added

--- script.js
+++ script.js
@@ -1,28 +1,16 @@
-
-"use script";
 const openBtn = document.getElementById("menu");
 // const openBtn = document.getElementById("menu");
 const closeBtn = document.getElementById("close__btn");
 const openMenu = document.getElementById("button");
 
-// const currentDate = document.getElementsByTagName("li");
+let openButton = function () {
+  openMenu.style.display = "block";
+};
 
-const currentDate = document.getElementsByTagName("li");
+openBtn.addEventListener("click", openButton);
 
-let hiii = document.getElementById("hiii");
+let closeButton = function () {
+  openMenu.style.display = "none";
+};
 
-const calendarLi = document.getElementsByClassName("click__li");
-
-currentDate.addEventListener("click", function () {
-  calendarLi.style.backgroundColor = "#0088F0";
-  calendarLi.style.color = "#fff";
-  calendarLi.style.borderRadius = "50%";
-  calendarLi.style.width = "20px";
-  calendarLi.style.height = "40px";
-
-  console.log("hello");
-});
-
-hiii.addEventListener("click", function () {
-  alert("hoooooo");
-});
+closeBtn.addEventListener("click", closeButton);
